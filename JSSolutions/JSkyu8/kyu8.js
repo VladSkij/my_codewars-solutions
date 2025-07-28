b2ee180a794c6ed980917954778ed12cd8d94a43
Vlad Skalski <vlad.skalsky.96@gmail.com>
2025-07-28 12:24:20 +0200

    Find unique number in array.

--- JSSolutions/JSkyu8/kyu8.js
+++ JSSolutions/JSkyu8/kyu8.js
@@ -87,3 +87,18 @@ for(let i = 0; i<languageDataBase.length; i++){
 }
   return 'Welcome'
 }
+
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+function findUniq(arr){
+  arr.sort((a,b) => a - b);
+  
+  if (arr[0] === arr[1]){
+    return arr[arr.length - 1];
+  } else return arr[0];
+
+}
