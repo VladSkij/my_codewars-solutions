61b3345c8aded34c81d0442f19306f7c422ee42f
Vlad Skalski <vlad.skalsky.96@gmail.com>
2025-07-28 15:15:29 +0200

    Return true if the array contains the value, false if not.

--- JSSolutions/JSkyu8/kyu8.js
+++ JSSolutions/JSkyu8/kyu8.js
@@ -137,3 +137,19 @@ function sum (numbers) {
 function validatePIN (pin) {
 return /^\d+$/.test(pin) && (pin.length=== 4 || pin.length=== 6)
 }
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// a can contain numbers or strings. x can be either.
+
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+  // your code here
+for (let i=0;i<a.length; i++){
+  if(a[i]==x){
+    return true
+  } 
+}
+return false
+}
