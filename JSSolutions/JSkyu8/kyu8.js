b5ce77ee0a84868a0c01f84e65eb0e6c872067ed
Vlad Skalski <vlad.skalsky.96@gmail.com>
2025-07-30 20:49:07 +0200

    Each character in string is repeated once

--- JSSolutions/JSkyu8/kyu8.js
+++ JSSolutions/JSkyu8/kyu8.js
@@ -237,3 +237,19 @@ function rowSumOddNumbers(n) {
 return n*n*n
 }
 
+/*Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__   */
+
+function doubleChar(str) {
+  // Your code here
+let newStr=[];
+  for(let i = 0; i<str.length; i++){
+    newStr +=(str[i]+str[i])
+  }
+  return newStr
+}
+
+
