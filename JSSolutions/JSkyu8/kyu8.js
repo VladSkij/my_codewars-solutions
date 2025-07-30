c13bc461fb248643fd32ff03ab9d7f7e79789e86
Vlad Skalski <vlad.skalsky.96@gmail.com>
2025-07-30 13:36:07 +0200

    Integer that appears an odd number of times

--- JSSolutions/JSkyu8/kyu8.js
+++ JSSolutions/JSkyu8/kyu8.js
@@ -170,3 +170,35 @@ function doubleInteger(i) {
   // i will be an integer. Double it and return it.
   return i*2;
 }
+
+/*Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+
+Examples
+[7] should return 7, because it occurs 1 time (which is odd).
+[0] should return 0, because it occurs 1 time (which is odd).
+[1,1,2] should return 2, because it occurs 1 time (which is odd).
+[0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+[1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd). */
+
+function findOdd(A) {
+  //happy coding!
+let sortArray = A.sort();
+let groups = [];
+  for(let i =0; i<sortArray.length; i++){
+    let current = sortArray[i];
+    let prev = sortArray[i-1];
+    if(current===prev){
+      groups[groups.length-1].push(current)
+    }else{
+      groups.push([current])
+    }
+  }
+  for(let i=0 ; i<groups.length; i++){
+    let group=groups[i]
+    if(group.length % 2 !== 0){
+      return group[0]
+    }
+  }
+}
