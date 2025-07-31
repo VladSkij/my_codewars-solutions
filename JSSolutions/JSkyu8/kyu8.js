98e78bd471833e5e82dd0017f6ae881030979439
Vlad Skalski <vlad.skalsky.96@gmail.com>
2025-07-31 13:27:16 +0200

    returns that single different number

--- JSSolutions/JSkyu8/kyu8.js
+++ JSSolutions/JSkyu8/kyu8.js
@@ -267,3 +267,17 @@ function abbrevName(name){
 let arr = name.split(" ")
     return (arr[0][0]+"."+arr[1][0]).toUpperCase()
 }
+
+/*You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3 */
+
+function stray(numbers) {
+return numbers.sort((a,b)=>a-b)[0]===numbers[1]?numbers[numbers.length-1]:numbers[0]
+}
