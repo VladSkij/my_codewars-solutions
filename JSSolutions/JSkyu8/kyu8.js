9ace706b2b04b8de85422e5d1301df00b40828e0
Vlad Skalski <vlad.skalsky.96@gmail.com>
2025-08-01 15:43:09 +0200

    Return correct string

--- JSSolutions/JSkyu8/kyu8.js
+++ JSSolutions/JSkyu8/kyu8.js
@@ -414,3 +414,11 @@ The first character in the string is always a number. */
 function getAge(inputString){
 return Number(inputString[0]);
 }
+
+/**Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+Example output: Hello, Mr. Spock */
+
+function sayHello(name) {
+  return ("Hello, "+name)
+}
