3f1beeaeebdd0b5df608d2f0eb52c7d4902d0434
Vlad Skalski <vlad.skalsky.96@gmail.com>
2025-08-04 22:54:52 +0200

    Pangram

--- JSSolutions/JSkyu8/kyu8.js
+++ JSSolutions/JSkyu8/kyu8.js
@@ -639,3 +639,12 @@ Return true if yes, false otherwise :) */
 function hero(bullets, dragons){
 return bullets/dragons>=2;
 }
+
+/**A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation. */
+
+function isPangram(string) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const lowerStr = string.toLowerCase();
+  return [...alphabet].every(letter => lowerStr.includes(letter));
+}
