0a74572b7966dad7dc2f00b2f8fc7be7a21af2fb
Vlad Skalski <vlad.skalsky.96@gmail.com>
2025-08-04 21:44:13 +0200

    Sorted array of string

--- JSSolutions/JSkyu8/kyu8.js
+++ JSSolutions/JSkyu8/kyu8.js
@@ -603,3 +603,14 @@ Note: base is a non-negative number, factor is a positive number. */
 function checkForFactor (base, factor) {
   return (base%factor)===0?true:false;
 }
+
+/**Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+For example, if this array were passed as an argument:
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+Your function would return the following array:
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length. */
+
+function sortByLength (array) {
+    return array.sort((a,b)=>a.length-b.length);
+}
