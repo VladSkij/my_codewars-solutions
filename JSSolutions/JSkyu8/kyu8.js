a6688b2446f57fe7351fe125a16f9588a70fad53
Vlad Skalski <vlad.skalsky.96@gmail.com>
2025-08-05 23:09:24 +0200

    Grade book +1more solution

--- JSSolutions/JSkyu8/kyu8.js
+++ JSSolutions/JSkyu8/kyu8.js
@@ -711,3 +711,8 @@ if(score>=90&&score<=100){
   return 'F'
 }
 }
+
+function getGrade (s1, s2, s3) {
+  var s = (s1 + s2 + s3) / 3
+  return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F"
+}
